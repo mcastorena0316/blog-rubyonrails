22fe34e03bb51c81e0f8916d567e3e90b68f48c9
Meli Castorena <mcastorena0316@gmail.com>
2019-12-10 12:53:06 -0600

    Add security and partial views comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,21 @@
 class CommentsController < ApplicationController
+    http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+   
     def create
-        @article = Article.find(params[:article_id])
-        @comment = @article.comments.create(comment_params)
-        redirect_to article_path(@article)
+      @article = Article.find(params[:article_id])
+      @comment = @article.comments.create(comment_params)
+      redirect_to article_path(@article)
     end
-        
-        private
-    def comment_params
+   
+    def destroy
+      @article = Article.find(params[:article_id])
+      @comment = @article.comments.find(params[:id])
+      @comment.destroy
+      redirect_to article_path(@article)
+    end
+   
+    private
+      def comment_params
         params.require(:comment).permit(:commenter, :body)
-    end
-
-end
+      end
+  end
